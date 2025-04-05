58434cb1cdd68c55ce45ba45cbd6e8887bff7525
Shubham <shubhamsm4175@gmail.com>
2025-04-06 00:59:06 +0400

    path join fix

--- backend/src/index.js
+++ backend/src/index.js
@@ -37,7 +37,7 @@ app.use("/api/auth", authRoute);
 app.use("/api/messages", messageRoute);
 
 if(process.env.NODE_ENV === "production"){
-    app.use(express.static(path.json(__direname,"../frontend/dist")));
+    app.use(express.static(path.join(__direname,"../frontend/dist")));
 
     app.get('*',(req,res)=>{
         res.sendFile(path.join(__direname,"..frontend","dist","index.html "))
